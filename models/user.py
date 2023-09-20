b14a016d25a82bc76168457b8f94f49e72a95b38
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-09-20 13:12:59 +0300

    solve task 7

--- models/user.py
+++ models/user.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+import models
+import sqlalchemy
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """class User"""
+
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship('Place', backref='user', cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
+
+
+    def __init__(self, *args, **kwargs):
+        """ init user """
+        super().__init__(*args, **kwargs)
